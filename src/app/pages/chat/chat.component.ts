8705d6755f8af10e77fdc8fc822df1941316a167
Eugene Hermano <eunico.hermano@gmail.com>
2024-07-23 20:34:47 +0800

    changes on UI and env var

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -54,8 +54,8 @@ export class ChatComponent {
   async initHubConnection() {
     try {
       this.hubConnection = new HubConnectionBuilder()
-        // .withUrl(environment.hubUrl)
-        .withUrl('chat/')
+        .withUrl(environment.hubUrl)
+        // .withUrl('chat/')
         .build();
 
       this.hubConnection.on('ReceiveAdminUpdate', (username, msg, time) => {
